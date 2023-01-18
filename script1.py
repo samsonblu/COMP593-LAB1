e8c7e741a640fab1a33c42012b8b11ffa0f7f554
sam <115283467+samsonblu@users.noreply.github.com>
2023-01-18 13:39:20 -0500

    Comment added to script1.py

--- script1.py
+++ script1.py
@@ -2,3 +2,4 @@ print("""Beautiful is better than ugly.
 Explicit is better than implicit.
 Simple is better than complex.
 Complex is better than complicated.""")
+#This is a comment, I have made a comment. So cool!
