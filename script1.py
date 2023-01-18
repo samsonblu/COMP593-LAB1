97824dc197d877d7287e3c64d1a7e17890d9dc87
sam <115283467+samsonblu@users.noreply.github.com>
2023-01-18 14:21:36 -0500

    Modified script1.py

--- script1.py
+++ script1.py
@@ -3,3 +3,5 @@ Explicit is better than implicit.
 Simple is better than complex.
 Complex is better than complicated.""")
 #This is a comment, I have made a comment. So cool!
+
+#Added change to the new release
