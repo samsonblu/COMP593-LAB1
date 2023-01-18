ffd6a240f4fd6b621ce8027dd24f95c35fd70b34
sam <115283467+samsonblu@users.noreply.github.com>
2023-01-18 13:42:24 -0500

    Modified both scripts

--- script1.py
+++ script1.py
@@ -1,5 +1,8 @@
 print("""Beautiful is better than ugly.
 Explicit is better than implicit.
 Simple is better than complex.
-Complex is better than complicated.""")
+Complex is better than complicated.
+
+The dutch.
+""")
 #This is a comment, I have made a comment. So cool!
